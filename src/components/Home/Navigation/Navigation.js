9f9b8861121cac2cbac8fa1cb4a6879e7dce9ade
Wilfredo R Colina <wrca26@gmail.com>
2022-08-03 11:41:04 -0500

    Nav stablished

--- src/components/Home/Navigation/Navigation.js
+++ src/components/Home/Navigation/Navigation.js
@@ -1,45 +1,62 @@
-import React,{useState} from "react";
+import React, {useState} from "react";
 import Box from "@mui/material/Box";
-import {ListItem, Stack} from "@mui/material";
-import {Item, UList, Wrapper, Logo, ContactLink, BoxIcon} from "./NavigationStyles";
+import {Stack, Grid} from "@mui/material";
+import {Item, UList, Wrapper, Logo, ContactLink, BoxIcon, Identifier, Burger, BurgerController} from "./NavigationStyles"
 import Link from "../../../Link"
 import Typography from "@mui/material/Typography";
 import TrendingFlatIcon from '@mui/icons-material/TrendingFlat';
 
 const Navigation = () => {
-const [logHover, isLogHover] = useState(false);
+    const [logHover, isLogHover] = useState(false);
+    const [isBurgerOpen, setIsBurgerOpen] = useState(false);
     return (<Wrapper>
         <UList>
-            <Box>
-                <Logo variant={"h5"}>
-                    Wilfredo Colina
-                </Logo>
-            </Box>
-
+            <Link href='/'>
+                <Box>
+                    <Logo variant={"h5"}>
+                        Wilfredo Colina
+                    </Logo>
+                </Box>
+            </Link>
             <Stack direction="row">
                 <Link href="/" color="secondary">
-                    <Item> Home </Item>
+                    <Item> Home <Identifier/> </Item>
+
                 </Link>
+
                 <Link href="/projects" color="secondary">
                     <Item> Projects </Item>
                 </Link>
-
                 <Link href="/about" color="secondary">
-                    <Item> About</Item>
+                    <Item> About </Item>
                 </Link>
+
             </Stack>
-            <Box onMouseEnter={()=>isLogHover(true)} onMouseLeave={()=>isLogHover(false)}>
-                <Link  href="/contact" color="secondary" >
-                <ContactLink direction="row">
-                    <BoxIcon hoverState={logHover}>
-                        <TrendingFlatIcon/>
-                    </BoxIcon>
-                    <Typography>
-                        Let's chat
-                    </Typography>
-                </ContactLink>
+            <Stack direction={'row'}>
+
+
+            <Box onMouseEnter={() => isLogHover(true)} onMouseLeave={() => isLogHover(false)}>
+                <Link href="/contact" color="secondary">
+                    <ContactLink direction="row">
+                        <BoxIcon hoverState={logHover}>
+                            <TrendingFlatIcon/>
+                        </BoxIcon>
+                        <Typography>
+                            Let's chat
+                        </Typography>
+                    </ContactLink>
                 </Link>
             </Box>
+            <BurgerController>
+                <Burger open={isBurgerOpen}
+                        onClick={()=>setIsBurgerOpen(!isBurgerOpen)}
+                >
+                    <div/>
+                    <div/>
+                    <div/>
+                </Burger>
+            </BurgerController>
+            </Stack>
         </UList>
     </Wrapper>)
 };
--- src/components/Home/Navigation/NavigationStyles.js
+++ src/components/Home/Navigation/NavigationStyles.js
@@ -1,13 +1,25 @@
 import {styled} from "@mui/material/styles";
-import {ListItem, List, Box, Stack} from "@mui/material";
+import {ListItem, List, Box, Stack, Grid} from "@mui/material";
 import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
 
 export const Item = styled(ListItem)`
   color: #000;
   margin-left: 1rem;
   font-size: 1.5rem;
+  display: flex;
+  justify-content: center;
+  ${props => props.theme.breakpoints.up("sm")} {
   
+  }
+  ${props => props.theme.breakpoints.down("md")} {
+    display: none;
+   
+  }
+  ${props => props.theme.breakpoints.up("lg")} {
+   
+  }
 `
 
 export const UList = styled(List)`
@@ -26,30 +38,34 @@ export const UList = styled(List)`
   }
 `
 
-export const Wrapper = styled(Box)`
+export const Wrapper = styled(Grid)`
   box-sizing: border-box;
   width: 100%;
   height: auto;
-  background-color:rgba(0,0,0,0.005);
+  background-color:rgba(0,0,0,0.006);
   top: 0;
   position: sticky;
   box-shadow: 0 2px 2px -2px rgba(0,0,0,.2);
 `
 
 export const Logo = styled(Typography)`
-  color: #000;
+  color: #069985;
+  text-shadow: 0 0 1px #000000;
   margin-left: 1rem;
   text-transform: uppercase;
   font-family: 'Inconsolata', monospace;
+  letter-spacing: -1.5px;
   font-weight: bold;
-  
-  `
+`
 
 export const ContactLink = styled(Stack)`
   color: #000;
   font-weight: 400;
   margin-right: 1rem;
+  ${props => props.theme.breakpoints.down("sm")} {
+    display: none;
 
+  }
 `
 
 export const BoxIcon = styled(props => (
@@ -68,4 +84,72 @@ export const BoxIcon = styled(props => (
     transition: .4ms ease-in-out;
     
   }
+`
+
+export const Identifier = styled('span')`
+  position: absolute;
+  background:#01B19C;
+  border: 1px solid #000;
+  border-radius: 60%;
+  height: 12px;
+  width: 12px;
+  top: 45px;
+`
+export const BurgerController = styled(Box)`
+  ${props => props.theme.breakpoints.up("md")} {
+    display: none;
+  }
+`
+
+export const Burger = styled((props)=>(
+    <Button{...props} />
+))`
+  
+  ${props => props.theme.breakpoints.down("md")} {
+   position: relative;
+  display: flex;
+  flex-direction: column;
+  cursor: pointer;
+  width: 24px;
+  line-height: 50px;
+  padding: 0;
+  background: transparent;
+  justify-content: center;
+  box-sizing: border-box;
+  border: none;
+  margin-right: 1.5rem;
+  z-index: 1000;
+  transition: transform 0.3s ease-in-out;
+  align-items: center;
+  color: #f3f3bd;
+  &:focus {
+    outline: none;
+  }
+  div {
+    width: 2rem;
+    height: 0.2rem;
+    background: ${(props) =>
+    props.open ? "#060505" : "#0b0b0b"};
+    transition: all 0.3s linear;
+    position: relative;
+    transform-origin: 3px;
+  }
+  div:first-of-type {
+    transform: ${(props) =>
+    props.open ? "rotate(45deg)" : "rotate(0)"};
+  }
+  div:nth-of-type(2) {
+    margin-top: 0.4rem;
+    opacity: ${(props) => (props.open ? "0" : "1")};
+    transform: ${(props) =>
+    props.open ? "translateX(20px)" : "translateX(0)"};
+  }
+  div:nth-of-type(3) {
+    margin-top: 0.4rem;
+    transform: ${(props) =>
+    props.open ? "rotate(-45deg)" : "rotate(0)"};
+  }
+
+  }
+  
 `
