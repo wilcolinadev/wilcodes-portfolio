eb506e4bee54435c9a9c67847a321ed252c6d3cf
Wilfredo R Colina <wrca26@gmail.com>
2022-09-30 10:16:20 -0500

    capturing data in the form and setting the state for validation

--- src/Components/Contact/Form/Form.js
+++ src/Components/Contact/Form/Form.js
@@ -1,17 +1,43 @@
-import React, {useState} from "react";
+import React, {useState, useRef} from "react";
 import {FormControl, Grid} from "@mui/material";
-import {ContactForm, FormWrapper, Button, FormDescription, FormInput, FormArea} from './FormStyles'
+import {ContactForm, FormWrapper, Button, FormDescription, FormInput, FormArea, FileLabel} from './FormStyles'
 import BoxIcon from "../../BoxIcon";
 import Typography from "@mui/material/Typography";
+import UploadFileOutlinedIcon from '@mui/icons-material/UploadFileOutlined';
 
 const Form = () => {
     const sxStyles = {
         width: '100%',
     }
     const [isButtonHover, setIsButtonHover] = useState(false);
+    const [isFormValid, setIsFormValid] = useState(false);
+
+    const nameRef = useRef(null);
+    const [name, setName] = useState('');
+
+    const lastNameRef = useRef(null);
+    const [lastName, setLastName] = useState('');
+
+    const companyRef = useRef(null);
+    const [company, setCompany] = useState('');
+
+    const emailRef = useRef(null);
+    const [email, setEmail] = useState('');
+
+    const messageRef = useRef(null);
+    const [message, setMessage] = useState('');
+
+
+    const [files, setFiles] = useState([]);
+
+    const handleFile = (e)=>{
+        const file = [...e.target.files];
+        setFiles((prevState)=>[...prevState,...file]);
+    }
+
     return (
         <FormWrapper>
-            <ContactForm>
+            <ContactForm >
                 <FormDescription variant={'h4'}>
                     Fill the Form Below!
                 </FormDescription>
@@ -19,28 +45,50 @@ const Form = () => {
                     <Grid item xs={12} md={6}>
                         <FormControl sx={sxStyles}>
                             <label>First Name</label>
-                            <FormInput type={'text'} placeholder={'Wilfredo'} required></FormInput>
+                            <FormInput name={'firstName'} onChange={() => setName(nameRef.current.value)} type={'text'}
+                                       placeholder={'John'} required value={name}
+                                       ref={nameRef}></FormInput>
                         </FormControl>
                     </Grid>
-                  <Grid item xs={12} md={6}>
-                      <FormControl sx={sxStyles}>
-                          <label>Last Name</label>
-                          <FormInput type={'text'} placeholder={'Colina'}></FormInput>
-                      </FormControl>
-                  </Grid>
+                    <Grid item xs={12} md={6}>
+                        <FormControl sx={sxStyles}>
+                            <label>Last Name</label>
+                            <FormInput name={'lastName'}
+                                       type={'text'}
+                                       placeholder={'Mauer'}
+                                       ref={lastNameRef}
+                                       value={lastName}
+                                       onChange={()=>setLastName(lastNameRef.current.value)}
+                            ></FormInput>
+                        </FormControl>
+                    </Grid>
 
                 </Grid>
 
                 <Grid>
                     <FormControl sx={sxStyles}>
                         <label>Company Name</label>
-                        <FormInput type={'text'} placeholder={'Company Name'}></FormInput>
+                        <FormInput
+                            name={'companyName'}
+                            type={'text'}
+                            placeholder={'Company Optional'}
+                            ref={companyRef}
+                            value={company}
+                            onChange={()=>setCompany(companyRef.current.value)}
+                        ></FormInput>
                     </FormControl>
                 </Grid>
                 <Grid>
                     <FormControl sx={sxStyles}>
                         <label>Email</label>
-                        <FormInput type={'email'} placeholder={'example@mail.com'}></FormInput>
+                        <FormInput
+                            name={'email'}
+                            type={'email'}
+                            placeholder={'example@mail.com'}
+                            ref={emailRef}
+                            value={email}
+                            onChange={()=>setEmail(emailRef.current.value)}
+                        ></FormInput>
                     </FormControl>
                 </Grid>
 
@@ -48,27 +96,40 @@ const Form = () => {
                     <FormControl sx={sxStyles}>
                         <label>Message</label>
                         <FormArea
+                            name={'message'}
                             minRows={5}
                             placeholder="Type your message here"
-                            sx={sxStyles}>
+                            sx={sxStyles}
+                            ref={messageRef}
+                            value={message}
+                            onChange={()=>setMessage(messageRef.current.value)}
+                        >
+
 
                         </FormArea>
                     </FormControl>
                 </Grid>
                 <Grid>
-                    <label htmlFor="choose-file">
+                    <FileLabel htmlFor="choose-file">
                         <input
-                            accept="image/*,.pdf"
+                            name={'files'}
+                            accept="image/*, .pdf"
                             id="choose-file"
                             multiple
                             type="file"
-
+                            hidden
+                            onChange={(e)=>handleFile(e)}
                         />
-                    </label>
+                        <UploadFileOutlinedIcon/>
+                        Upload file
+
+                    </FileLabel>
                 </Grid>
                 <Grid container justifyContent="center" alignItems="center">
                     <Button type="submit" onMouseEnter={() => setIsButtonHover(!isButtonHover)}
-                            onMouseLeave={() => setIsButtonHover(!isButtonHover)}>
+                            onMouseLeave={() => setIsButtonHover(!isButtonHover)}
+                            disabled={isFormValid}
+                    >
                         <BoxIcon hoverState={isButtonHover}/>
                         <Typography>
                             Just Send!
--- src/Components/Contact/Form/FormStyles.js
+++ src/Components/Contact/Form/FormStyles.js
@@ -17,8 +17,8 @@ export const ContactForm = styled('form')`
     font-weight: 400;
     font-size: 1.2rem;
     margin:1rem 0;
+    color: #3F3D55;
   }
-  
   
 `;
 
@@ -28,23 +28,29 @@ font-family: 'inconsolata', monospace;
 `
 
 export const FormWrapper = styled(Box)`
-  background-color: rgba(255, 255, 255, 0.8);
+  background-color: rgba(255, 255, 255, 0.91);
   border-top: 2px solid #3F3D55;
   padding: 2rem;
+
   ${props => props.theme.breakpoints.up("md")} {
     margin-top: 2.5rem;
     padding: 0;
   }
-  
- 
+
+
 `
-export const Button = styled('button')`
+export const Button = styled(props => (
+    <button {...props} />
+
+))`
   background-color: transparent;
   border: none;
   display: flex;
   align-items: center;
   cursor: pointer;
   margin: 1rem 0 2rem;
+  
+  
 `
 export const FormInput = styled("input")`
   font-family: 'inconsolata', monospace;
@@ -73,4 +79,12 @@ export const FormArea = styled('textarea')`
     outline: none !important;
     box-shadow: 0 0 5px #5b9675;
   }
+`
+
+export const FileLabel = styled('label')`
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  text-decoration: underline;
+  padding: .5rem 0;
 `
