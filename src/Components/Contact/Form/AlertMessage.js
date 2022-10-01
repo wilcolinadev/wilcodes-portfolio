6f6ebb036b7f055ffd71b9e70618e8fb692b060b
Wilfredo R Colina <wrca26@gmail.com>
2022-10-01 11:26:58 -0500

    Alert message for error hanlding created

---
+++ src/Components/Contact/Form/AlertMessage.js
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+const AlertMessage = ({error, success, setError, setSuccess}) => {
+
+
+    return (
+        <Stack spacing={2} sx={{width: '30%', margin: '0 auto'}}>
+
+            <Snackbar open={success}
+                      anchorOrigin={{vertical: 'bottom', horizontal: 'center'}}
+                      onClose={() => setSuccess(false)}
+                      autoHideDuration={3000}
+            >
+                <Alert severity="success">
+                    Message Sent succesfully!
+                </Alert>
+            </Snackbar>
+
+            <Snackbar open={error}
+                      anchorOrigin={{vertical: 'bottom', horizontal: 'center'}} onClose={() => setError(false)}
+                      autoHideDuration={3000}>
+                <Alert severity="error">
+                  Server Erro, Try again!
+                </Alert>
+            </Snackbar>
+        </Stack>
+    );
+}
+export default AlertMessage;
--- src/Components/Contact/Form/Form.js
+++ src/Components/Contact/Form/Form.js
@@ -6,7 +6,7 @@ import Typography from "@mui/material/Typography";
 import UploadFileOutlinedIcon from '@mui/icons-material/UploadFileOutlined';
 import {formValidation} from "./formValidation";
 import axios from "axios";
-import * as emailjs from "@emailjs/browser";
+import CustomizedSnackbars from "./AlertMessage";
 
 const Form = () => {
 
@@ -31,19 +31,22 @@ const Form = () => {
     const messageRef = useRef(null);
     const [message, setMessage] = useState('');
 
+    const [error, setError] = useState(false);
+    const [successResponse, setsuccessResponse] = useState(false);
+
 
     const [files, setFiles] = useState([]);
 
-    const handleFile = (e)=>{
+    const handleFile = (e) => {
         const file = [...e.target.files];
-        setFiles((prevState)=>[...prevState,...file]);
+        setFiles((prevState) => [...prevState, ...file]);
     }
 
     useEffect(() => {
-        setIsFormValid(formValidation(name, lastName,email,company))
+        setIsFormValid(formValidation(name, lastName, email, company))
     }, [name, lastName, email, company]);
 
-    const sendForm  = async (e)=>{
+    const sendForm = async (e) => {
         e.preventDefault();
         const data = {
             service_id: 'service_c7mmf6g',
@@ -51,127 +54,136 @@ const Form = () => {
             user_id: 'user_SVVnZkO5DWGt5scPKPwAx',
             template_params: {
                 'name': name,
-                'lastName' :lastName,
-                'company':company,
-                'email':email,
-                'message':message
+                'lastName': lastName,
+                'company': company,
+                'email': email,
+                'message': message
             }
         };
-        let response = await axios.post('https://api.emailjs.com/api/v1.0/email/send', data)
-        console.log(response);
+
+        try {
+            let response = await axios.post('https://api.emailjs.com/api/v1.0/email/send', data);
+            console.log(response);
+            await setsuccessResponse(!successResponse);
+        } catch (e) {
+            console.log(e);
+            setError(!error);
+        }
+
+
     }
 
 
-
-
-
-        return (
-            <FormWrapper>
-                <ContactForm onSubmit={sendForm}>
-                    <FormDescription variant={'h4'}>
-                        Fill the Form Below!
-                    </FormDescription>
-                    <Grid container spacing={3}>
-                        <Grid item xs={12} md={6}>
-                            <FormControl sx={sxStyles}>
-                                <label>First Name</label>
-                                <FormInput name={'firstName'} onChange={() => setName(nameRef.current.value)}
-                                           type={'text'}
-                                           placeholder={'John'} required value={name}
-                                           ref={nameRef}></FormInput>
-                            </FormControl>
-                        </Grid>
-                        <Grid item xs={12} md={6}>
-                            <FormControl sx={sxStyles}>
-                                <label>Last Name</label>
-                                <FormInput name={'lastName'}
-                                           type={'text'}
-                                           placeholder={'Mauer'}
-                                           ref={lastNameRef}
-                                           value={lastName}
-                                           onChange={() => setLastName(lastNameRef.current.value)}
-                                ></FormInput>
-                            </FormControl>
-                        </Grid>
-
-                    </Grid>
-
-                    <Grid>
+    return (
+        <FormWrapper>
+            <ContactForm onSubmit={sendForm}>
+                <FormDescription variant={'h4'}>
+                    Fill the Form Below!
+                </FormDescription>
+                <Grid container spacing={3}>
+                    <Grid item xs={12} md={6}>
                         <FormControl sx={sxStyles}>
-                            <label>Company Name</label>
-                            <FormInput
-                                name={'companyName'}
-                                type={'text'}
-                                placeholder={'Company Optional'}
-                                ref={companyRef}
-                                value={company}
-                                onChange={() => setCompany(companyRef.current.value)}
-                            ></FormInput>
+                            <label>First Name</label>
+                            <FormInput name={'firstName'} onChange={() => setName(nameRef.current.value)}
+                                       type={'text'}
+                                       placeholder={'John'} required value={name}
+                                       ref={nameRef}></FormInput>
                         </FormControl>
                     </Grid>
-                    <Grid>
+                    <Grid item xs={12} md={6}>
                         <FormControl sx={sxStyles}>
-                            <label>Email</label>
-                            <FormInput
-                                name={'email'}
-                                type={'email'}
-                                placeholder={'example@mail.com'}
-                                ref={emailRef}
-                                value={email}
-                                onChange={() => setEmail(emailRef.current.value)}
+                            <label>Last Name</label>
+                            <FormInput name={'lastName'}
+                                       type={'text'}
+                                       placeholder={'Mauer'}
+                                       ref={lastNameRef}
+                                       value={lastName}
+                                       onChange={() => setLastName(lastNameRef.current.value)}
                             ></FormInput>
                         </FormControl>
                     </Grid>
 
-                    <Grid>
-                        <FormControl sx={sxStyles}>
-                            <label>Message</label>
-                            <FormArea
-                                name={'message'}
-                                minRows={5}
-                                placeholder="Type your message here"
-                                sx={sxStyles}
-                                ref={messageRef}
-                                value={message}
-                                onChange={() => setMessage(messageRef.current.value)}
-                            >
+                </Grid>
 
+                <Grid>
+                    <FormControl sx={sxStyles}>
+                        <label>Company Name</label>
+                        <FormInput
+                            name={'companyName'}
+                            type={'text'}
+                            placeholder={'Company Optional'}
+                            ref={companyRef}
+                            value={company}
+                            onChange={() => setCompany(companyRef.current.value)}
+                        ></FormInput>
+                    </FormControl>
+                </Grid>
+                <Grid>
+                    <FormControl sx={sxStyles}>
+                        <label>Email</label>
+                        <FormInput
+                            name={'email'}
+                            type={'email'}
+                            placeholder={'example@mail.com'}
+                            ref={emailRef}
+                            value={email}
+                            onChange={() => setEmail(emailRef.current.value)}
+                        ></FormInput>
+                    </FormControl>
+                </Grid>
 
-                            </FormArea>
-                        </FormControl>
-                    </Grid>
-                    <Grid>
-                        <FileLabel htmlFor="choose-file">
-                            <input
-                                name={'files'}
-                                accept="image/*, .pdf"
-                                id="choose-file"
-                                multiple
-                                type="file"
-                                hidden
-                                onChange={(e) => handleFile(e)}
-                            />
-                            <UploadFileOutlinedIcon/>
-                            Upload files
-
-                        </FileLabel>
-                    </Grid>
-                    <Grid container justifyContent="center" alignItems="center">
-                        <Button type="submit" onMouseEnter={() => setIsButtonHover(!isButtonHover)}
-                                onMouseLeave={() => setIsButtonHover(!isButtonHover)}
-                                disabled={!isFormValid}
+                <Grid>
+                    <FormControl sx={sxStyles}>
+                        <label>Message</label>
+                        <FormArea
+                            name={'message'}
+                            minRows={5}
+                            placeholder="Type your message here"
+                            sx={sxStyles}
+                            ref={messageRef}
+                            value={message}
+                            onChange={() => setMessage(messageRef.current.value)}
                         >
-                            <BoxIcon hoverState={isButtonHover}/>
-                            <Typography>
-                                Just Send!
-                            </Typography>
-                        </Button>
 
-                    </Grid>
-                </ContactForm>
-            </FormWrapper>
 
-        )
+                        </FormArea>
+                    </FormControl>
+                </Grid>
+                <Grid>
+                    <FileLabel htmlFor="choose-file">
+                        <input
+                            name={'files'}
+                            accept="image/*, .pdf"
+                            id="choose-file"
+                            multiple
+                            type="file"
+                            hidden
+                            onChange={(e) => handleFile(e)}
+                        />
+                        <UploadFileOutlinedIcon/>
+                        Upload files
+
+                    </FileLabel>
+                </Grid>
+                <Grid container justifyContent="center" alignItems="center">
+                    <Button type="submit" onMouseEnter={() => setIsButtonHover(!isButtonHover)}
+                            onMouseLeave={() => setIsButtonHover(!isButtonHover)}
+                            disabled={!isFormValid}
+                    >
+                        <BoxIcon hoverState={isButtonHover}/>
+                        <Typography>
+                            Just Send!
+                        </Typography>
+                    </Button>
+
+                </Grid>
+            </ContactForm>
+
+            <CustomizedSnackbars error={error} success={successResponse} setError={setError}
+                                 setSuccess={setsuccessResponse}/>
+        </FormWrapper>
+
+    )
 
 };
 
